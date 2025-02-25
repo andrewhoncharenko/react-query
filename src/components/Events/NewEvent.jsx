affb357fec6e907b9ed4bd06cdc11cd5cb81f75a
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-02-25 14:41:28 +0200

    delete event

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -3,16 +3,28 @@ import { useMutation } from "@tanstack/react-query";
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const {mutate, isPending, isError, error} = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["events"]});
+      navigate("/events");
+    }
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({event: formData});
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
+        {isPending && "Submitting..."}
+        {!isPending && <>
           <Link to="../" className="button-text">
             Cancel
           </Link>
@@ -20,7 +32,9 @@ export default function NewEvent() {
             Create
           </button>
         </>
+        }
       </EventForm>
+      {isError && <ErrorBlock title="Failed to creagte event." message={error.info?.message || "Failed to creagte event. Please check your input and try again."}/>}
     </Modal>
   );
 }
